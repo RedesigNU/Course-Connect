2c8a697f336ee9520840a5058ad2965761036579
yulunwu1 <yulunwu2013@u.northwestern.edu>
2014-04-25 19:04:34 -0500

    algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,7 @@
+//assume dept_tbl is our department table
+//assume student object with two tables
+//assume we have an enrolled table
+//dept num
+//assume we have a taken table
+//dept num gpa
+//eecs 111 3.3
