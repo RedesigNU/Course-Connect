78c72b8b2ece4257a01b3e2e450f242f3baeb2da
yulunwu1 <yulunwu2013@u.northwestern.edu>
2014-04-25 19:12:45 -0500

    no message

--- algorithm.js
+++ algorithm.js
@@ -5,3 +5,16 @@
 //assume we have a taken table
 //dept num gpa
 //eecs 111 3.3
+
+function predict_classes (dept_tbl, student_enrolled, student_taken, requested_dpt)
+{
+	//first build student description
+	for (row i = 0; i < student_taken.length; i++)
+	{
+
+	}
+	for (row i=0;i<dept_tbl.length;i++)
+	{
+
+	}
+}
